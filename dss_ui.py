fe51f6b369e348a4524a8eb80f1b49c7f0124758
Galen Curwen-McAdams <galencm@fastmail.com>
2018-02-12 11:56:06 +0000

    Problem: no selection grid for image
    Solution: add simple grid with select
    and deselect

--- dss_ui.py
+++ dss_ui.py
@@ -12,6 +12,7 @@ from kivy.core.image import Image as CoreImage
 from kivy.core.window import Window
 from kivy.config import Config
 from kivy.graphics.vertex_instructions import Rectangle
+from kivy.graphics import Color, Line, Ellipse, InstructionGroup
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
@@ -31,46 +32,136 @@ Config.read('config.ini')
 
 class ClickableImage(Image):
     def __init__(self, **kwargs):
+        self.rows = 0
+        self.cols = 0
+        self.row_spacing = 100
+        self.col_spacing = 100
+        self.geometry = []
+        self.selected = InstructionGroup()
         super(ClickableImage, self).__init__(**kwargs)
 
+    def draw_rows(self):
+        pass
+
+    def draw_cols(self):
+        pass
+
+    def clear_grid(self):
+        self.canvas.remove_group('selections')
+
+    def draw_grid(self):
+        w, h = self.texture_size
+        if self.cols is None:
+            self.col_spacing = w
+
+        if self.rows is None:
+            self.row_spacing = w
+        # this will clear image too
+        # self.canvas.clear()
+        with self.canvas:
+            Color(128, 128, 128, 0.5)
+
+            if self.cols is not None:
+                for col in range(0, h, self.col_spacing):
+                    Line(points=[col, 0, col, h], width=2)
+
+            if self.rows is not None:
+                for row in range(0, w, self.row_spacing):
+                    Line(points=[0, row, w, row], width=2)
+
+    def draw_geometry(self):
+        self.clear_grid()
+
+        with self.canvas:
+            Color(128, 128, 128, 0.5)
+            for rect in self.geometry:
+                x,y,w,h = rect
+                Rectangle(pos=(x,y), size=(w, h),group='selections')
+
+    def draw_grid_click(self, x, y):
+        w, h = self.texture_size
+        #with self.canvas:
+        Color(128, 128, 128, 0.5)
+        dotsize = 10
+        with self.canvas:
+            Ellipse(pos=(x - (dotsize / 2), y - (dotsize / 2)), size=(dotsize, dotsize))
+        for col in range(0, w, self.col_spacing):
+            for row in range(0, h, self.row_spacing):
+                if col < x and x < col + self.col_spacing:
+                    if row < y and y < row + self.row_spacing:
+                        rect = (col, row, self.col_spacing, self.row_spacing)
+                        if rect not in self.geometry:
+                            with self.canvas:
+                                Rectangle(pos=(col,row), size=(self.col_spacing, self.row_spacing), group="selections")
+                            self.geometry.append(rect)
+                        else:
+                            self.geometry.remove(rect)
+                            self.draw_geometry()
+
+    def draw_grid_click_line(self, x, y, axis):
+        w, h = self.texture_size
+
+        if axis == "x":
+            for c in range(0, h, self.row_spacing):
+                self.draw_grid_click(c + 2, y)
+
+        if axis == "y":
+            for c in range(0, h, self.col_spacing):
+                self.draw_grid_click(x, c + 2)
+
+    def handle_keybinds(self, key):
+        # this will cause problems typing
+        # check that no focus on text input 
+        # widgets
+        if key == "spacebar":
+            self.geometry = []
+            self.clear_grid()
+
     def on_touch_up(self, touch):
-        print(touch.button)
-        if touch.button == 'right':
-            p = touch.pos 
-            o = touch.opos 
-            s = min(p[0],o[0]), min(p[1],o[1]), abs(p[0]-o[0]), abs(p[1]-o[1])
-            s = min(p[0],o[0]), min(p[1],o[1]), abs(p[0]-o[0]), abs(p[1]-o[1])
-            w  = s[2]
-            h  = s[3]
-            sx = s[0]
-            sy = s[1]
-            if abs(w) > 5 and abs(h) >5:
-                if self.collide_point(touch.pos[0],touch.pos[1]):
-                    #self.add_widget(Selection(pos=(sx,sy),size=(w, h)))
-                    #self.canvas.add(Rectangle(pos=(sx,sy),size=(w, h),color=(155,155,155,0.5)))
+        if touch.button == 'left':
+            self.draw_grid_click(touch.x, touch.y)
+        elif touch.button == 'right':
+            self.draw_grid_click_line(touch.x, touch.y, "x")
+        elif touch.button == 'middle':
+            self.draw_grid_click_line(touch.x, touch.y, "y")
 
-                    #because getting resized image no knowldge of original geometry
-                    #for scaling...
+        # clear all selected
 
-                    print("added widget for ",self)
-                    print(self.texture_size,self.norm_image_size,self.size)
-                    # width_scale  = self.texture_size[0] / self.norm_image_size[0]
-                    # height_scale = self.texture_size[1] / self.norm_image_size[1]
-                    x = touch.opos[0]
-                    y = abs(touch.opos[1]-self.norm_image_size[1])
-                    w = touch.pos[0]-touch.opos[0]
-                    h =abs(touch.pos[1]-self.norm_image_size[1])-abs(touch.opos[1]-self.norm_image_size[1])
+            # p = touch.pos 
+            # o = touch.opos 
+            # s = min(p[0],o[0]), min(p[1],o[1]), abs(p[0]-o[0]), abs(p[1]-o[1])
+            # s = min(p[0],o[0]), min(p[1],o[1]), abs(p[0]-o[0]), abs(p[1]-o[1])
+            # w  = s[2]
+            # h  = s[3]
+            # sx = s[0]
+            # sy = s[1]
+            # if abs(w) > 5 and abs(h) >5:
+            #     if self.collide_point(touch.pos[0],touch.pos[1]):
+            #         #self.add_widget(Selection(pos=(sx,sy),size=(w, h)))
+            #         #self.canvas.add(Rectangle(pos=(sx,sy),size=(w, h),color=(155,155,155,0.5)))
 
-                    print(x,y,w,h)
-                    px = x / self.norm_image_size[0]
-                    py = y / self.norm_image_size[1]
-                    pw = w / self.norm_image_size[0]
-                    ph = h / self.norm_image_size[1]
+            #         #because getting resized image no knowldge of original geometry
+            #         #for scaling...
 
-                    # print(Window.size)
-                    print(px,py,pw,ph)
-                    print(px*self.norm_image_size[0],py*self.norm_image_size[1],pw*self.norm_image_size[0],ph*self.norm_image_size[1])
-                    percents= "{} {} {} {}".format(px,py,pw,ph)
+            #         print("added widget for ",self)
+            #         print(self.texture_size,self.norm_image_size,self.size)
+            #         # width_scale  = self.texture_size[0] / self.norm_image_size[0]
+            #         # height_scale = self.texture_size[1] / self.norm_image_size[1]
+            #         x = touch.opos[0]
+            #         y = abs(touch.opos[1]-self.norm_image_size[1])
+            #         w = touch.pos[0]-touch.opos[0]
+            #         h =abs(touch.pos[1]-self.norm_image_size[1])-abs(touch.opos[1]-self.norm_image_size[1])
+
+            #         print(x,y,w,h)
+            #         px = x / self.norm_image_size[0]
+            #         py = y / self.norm_image_size[1]
+            #         pw = w / self.norm_image_size[0]
+            #         ph = h / self.norm_image_size[1]
+
+            #         # print(Window.size)
+            #         print(px,py,pw,ph)
+            #         print(px*self.norm_image_size[0],py*self.norm_image_size[1],pw*self.norm_image_size[0],ph*self.norm_image_size[1])
+            #         percents= "{} {} {} {}".format(px,py,pw,ph)
                     
         return super().on_touch_up(touch)
 
@@ -124,6 +215,13 @@ class TabItem(TabbedPanelItem):
                     except IndexError as ex:
                         self.root.switch_to(self.parent.children[0], do_scroll=True)
                         break
+        elif keycode[1] == 'spacebar':
+            for i, c in enumerate(self.parent.children):
+                if c == self.root.current_tab:
+                    try:
+                        c.keybindings.handle_keybinds("spacebar")
+                    except Exception as ex:
+                        print(ex)
         elif keycode[1] == 'c' and 'ctrl' in modifiers:
             App.get_running_app().stop()
 
@@ -168,13 +266,15 @@ class ChecklistApp(App):
         img = ClickableImage(
                              allow_stretch=True,
                              keep_ratio=True)
-        img.texture = CoreImage(data, ext="jpg").texture
+        img.texture = CoreImage(data, ext="jpg", keep_data=True).texture
+        img.draw_grid()
 
         tab = TabItem(text="overview",root=root)
         root.add_widget(tab)
 
         tab = TabItem(text="image",root=root)
         tab.add_widget(img)
+        tab.keybindings = img
         root.add_widget(tab)
 
         tab = TabItem(text="categories",root=root)
